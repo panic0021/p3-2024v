f9589899cf477e7b670e867bdceacedf5cd3fc58
panic0021 <panic.veljko@gmail.com>
2025-08-22 03:24:57 +0200

    lab2 pola

--- lab2/lab2_start.py
+++ lab2/lab2_start.py
@@ -1,46 +1,63 @@
 #%%
-# Zadatak 1
+# Zadatak 1 
+def concat_index_wise(l1,l2):
+    if len(l1)!=len(l2):
+        print("Neispravne liste unete, mora biti jednak broj stringova!")
+        return
+    duzina=len(l1)
+    l3=[]
+    for i in range(0,duzina,1):
+        l3.append(l1[i]+l2[i])
+    return l3
 
 
 
 
 #%%
-# Test the function
-# list1 = ["M", "na", "i", "Ke"]
-# list2 = ["y", "me", "s", "lly"]
-# print(concat_index_wise(list1, list2))
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+#print(concat_index_wise(list1, list2))
 
 #%%
 # Zadatak 2
 
-
+def digits_in_string(s):
+    numbers=[]
+    for i in range(0,len(s),1):
+        if(s[i].isdigit()):
+            numbers.append(int(s[i]))
+    return numbers
 
 
 #%%
 # Test the function
-# s1 = "Tokyo's 2024 population is now estimated at 37,115,035."
-# s2 = "Tokyo is one of the most populated cities."
-# print(digits_in_string(s1))
-# print(digits_in_string(s2))
-
+s1 = "Tokyo's 2024 population is now estimated at 37,115,035."
+s2 = "Tokyo is one of the most populated cities."
+#print(digits_in_string(s1))
+##print(digits_in_string(s2))
+#
 #%%
 # Zadatak 3
-
-
-
+def palindrom(s):
+    s=s.replace(" ","").replace(",","").replace(".","").replace("!","").replace("?","").lower()
+    for i in range(0,len(s),1):
+        if(s[i]!=s[len(s)-i-1]):
+            return False
+    return True
 
 #%%
-# Test the function
+
 # s1 = "potop"
 # print(f"{s1}: {palindrom(s1)}")
-# s2 = "Sir ima miris"
+# s2 = "Si... !!  !,,,.,., .,,.r ima miris!!!!!!!!!!!         !"
 # print(f"{s2}: {palindrom(s2)}")
 # s3 = "ananas"
 # print(f"{s3}: {palindrom(s3)}")
 
 #%%
 # Zadatak 4
-
+def password_check(lozinke):
+    
 
 
 
