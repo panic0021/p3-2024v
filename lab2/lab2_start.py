0264e4ebbb85e722d08978f7455757a716c440cb
panic0021 <panic.veljko@gmail.com>
2025-08-24 09:54:01 +0200

    lab2 zavrsen

--- lab2/lab2_start.py
+++ lab2/lab2_start.py
@@ -57,55 +57,107 @@ def palindrom(s):
 #%%
 # Zadatak 4
 def password_check(lozinke):
-    
-
-
+    password_list=[]
+    for word in lozinke.split(","):
+        password_list.append(word.strip())
+    valid_passwords = []
+    for word in password_list:
+        if not(len(word)>=6 and len(word)<=12):continue
+        par=[False]*4
+        for c in word:
+            if c.islower():par[0]=True
+            if c.isdigit():par[1]=True
+            if c.isupper():par[2]=True
+            if c in "$#@":par[3]=True
+            if len(c)>=6 and len(c)<=12:par[4]:True
+        if all(par):
+            valid_passwords.append(word)
+    print("Validne lozinke su: "+", ".join(valid_passwords))
 
 #%%
 # Test the function
 # passwords_to_check = "ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456WR"
 # print(f"Passwords to check: {passwords_to_check}")
-# passwords_check(passwords_to_check)
+#
+# password_check(passwords_to_check)
 
 
 #%%
 # Zadatak 5
-
-
+def server_status(s):
+    rows=[p.strip() for p in s.split("\n")]
+    rows=rows[1:-1]
+    distinct_servers={}
+    for r in rows:
+        #print(r)
+        row_words=r.split(" ")
+        distinct_servers[row_words[1]]=row_words[3]
+        # for i in range(0,len(row_words),1):
+        #     print(f"{row_words[i]} and {i}")
+    ctr=0
+    down_servers=[]
+    for item in distinct_servers:
+        #print(f"{item} : {distinct_servers[item]}")
+        if(distinct_servers[item]=="down"):
+            down_servers.append(item)
+            ctr+=1
+    down_servers_num=ctr/len(distinct_servers)
+    print(f'''
+    Ukupan broj servera: {len(distinct_servers)}
+    Procenat servera koji ne rade: {down_servers_num*100:.2f}%
+    Serveri koji ne rade: {(", ").join(down_servers)}
+    ''')
 
 
 #%%
 # Test the function
-# server_state_log = '''
-#     Server abc01 is up
-#     Server abc02 is down
-#     Server abc03 is down
-#     Server xyz01 is up
-#     Server xyz02 is up
-#     Server abc02 is up
-#     Server abc01 is down
-#     '''
-# server_status(server_state_log)
+server_state_log = '''
+    Server abc01 is up
+    Server abc02 is down
+    Server abc03 is down
+    Server xyz01 is up
+    Server xyz02 is up
+    Server abc02 is up
+    Server abc01 is down
+    Server xyz02 is down
+    Server Veljko is down
+    '''
+#server_status(server_state_log)
 
 
 #%%
 # Zadatak 6
+def anagram(s1,s2):
+    l1=[ch.lower() for ch in s1 if ch.isalpha()]
+    l2=[ch.lower() for ch in s2 if ch.isalpha()]
+    removed=0
+    for ch in l1:
+        if(ch in l2):
+            l2.remove(ch)
+            removed+=1
+    if(len(l2)==0 and len(l1)==removed):
+        print("Jeste anagram!")
+    else:
+        print("Nije anagram!")
+        
 
-
-
-
-#%%
-# Test the function
+# #%%
 # print(anagram('ortoped', 'torpedo'))
 # print(anagram('ortopedi', 'torpedo'))
 # print(anagram('On sa tla Like', 'Nikola Tesla'))
 
 #%%
 # Zadatak 7
+def all_even_digits():
+    num_list=[]
+    start=100
+    for i in range(start,401,1):
+        if((i//100)%2==0 and ((i//10)%10)%2==0 and (i%10)%2==0):
+            num_list.append(str(i))
 
-
+    print((", ").join(num_list))
 
 
 #%%
 # Test the function
-# all_even_digits()
+all_even_digits()
